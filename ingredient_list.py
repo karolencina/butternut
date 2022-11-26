a76606543c597ac147b206503ffe3e735255b862
Karolina Lencina <karolencina@icloud.com>
2022-11-26 18:49:04 +0000

    Create lists of db entries and set up db connection

---
+++ ingredient_list.py
@@ -0,0 +1,59 @@
+
+
+import sqlite3
+
+ingredients = [
+  "apple",
+  "banana",
+  "dill",
+  "egg",
+  "flour",
+  "granola",
+  "honey",
+  "lemon",
+  "onion",
+  "potato",
+  "rosemary",
+  "salt",
+  "thyme",
+  "vinegar",
+  "watermelon",
+  "pear",
+  "cucumber",
+  "garlic",
+  "carrot",
+  "eggplant",
+  "rice",
+  "lentils",
+  "lime",
+  "broth",
+  "mushrooms",
+  "orange",
+  "lettuce",
+  "cheese",
+  "cilantro"
+]
+
+recipes = [
+  ("Hummus", "easy", 30, 1, "Mix chickpea and tahini together. Add garlic and olive oil."),
+  ("Toast", "easy", 20, 1, "Cut a slice of bread. Top it with cream cheese, sun-dried tomatoes, and rocket.")
+]
+
+ingredients = sorted(ingredients)
+
+connection = sqlite3.connect("recipes.db")
+cursor = connection.cursor()
+
+cursor.execute("create table ingredients (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+cursor.execute("create table recipes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, img BLOB, level TEXT, duration_min INTEGER, servings INTEGER, instructions TEXT)")
+
+for i in range(len(ingredients)):
+  cursor.execute("insert into ingredients (name) values (?)",[ingredients[i]])
+  print("added ", ingredients[i])
+
+cursor.executemany("insert into recipes (name, level, duration_min, servings, instructions) values (?,?,?,?,?)", recipes)
+print("added ", recipes)
+
+
+connection.commit()
+connection.close()
